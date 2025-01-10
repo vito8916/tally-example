535affacbaa87833f863bf10d0d23e5002b6827b
Victor Alvarado <vito8916@gmail.com>
2025-01-09 20:28:48 -0600

    refactor main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,21 @@ export default function Home() {
         <section className="bg-white dark:bg-gray-900 h-full flex items-center justify-center">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-              Tally Form Example.
+              Tally Form Example - FAFSA Workshop &#34;Tu camino hacia la ayuda financiera&#34;
             </h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-             This is an example of a form embedded in a website or an external form. It is a simple form with a few fields and a submit
+             This is an example of a form embedded in a website. It is a simple form with a few fields and a submit
               button. It is designed to be used as an example of how to create a form that can be embedded in a website.
             </p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-              <a href="https://tally.so/r/waKEY9" target={"_blank"} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Go to external form
-                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"></path>
-                </svg>
-              </a>
-              <Link href="/embed"
+
+              <Link href="/mimic?email=test@test.com&name=John&surname=Doe"
                     className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 <svg className="mr-3 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                   <path
                       d="M1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12ZM12.0003 17C14.7617 17 17.0003 14.7614 17.0003 12C17.0003 9.23858 14.7617 7 12.0003 7C9.23884 7 7.00026 9.23858 7.00026 12C7.00026 14.7614 9.23884 17 12.0003 17ZM12.0003 15C10.3434 15 9.00026 13.6569 9.00026 12C9.00026 10.3431 10.3434 9 12.0003 9C13.6571 9 15.0003 10.3431 15.0003 12C15.0003 13.6569 13.6571 15 12.0003 15Z"></path>
                 </svg>
-                Watch embedded form
+                See the example
               </Link>
             </div>
           </div>
